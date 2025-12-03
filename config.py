520f7a32e25c161254dc4200c4a345f36737dda7
greenhornet63603 <greenhornet63603@gmail.com>
2025-12-03 17:54:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
-
 import os
 
 class Config(object):
-    # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8212010986:AAGeDz9VOqjD9-up_1AQ6dwCEKh-csq136s")
     API_ID = int(os.environ.get("API_ID", "27400172"))
     API_HASH = os.environ.get("API_HASH", "56d0a75c5f9a9de6beb5452aa63c2d36")
     AUTH_USERS = os.environ.get("AUTH_USERS", "7540570087")
+
